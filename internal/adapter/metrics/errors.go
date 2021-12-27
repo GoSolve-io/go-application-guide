d7060e972ab278fd15923a26b3be5ee7bded24e5
ejdem86 <ejdem86@gmail.com>
2021-12-27 21:58:34 +0100

    Added sample metrics provider

--- internal/adapter/metrics/errors.go
+++ internal/adapter/metrics/errors.go
@@ -3,8 +3,8 @@ package metrics
 import "errors"
 
 var (
-	// ErrInvalidTags means the provided tags are not valid
+	// ErrInvalidTags means the provided tags are not valid.
 	ErrInvalidTags = errors.New("invalid tags")
-	// ErrInvalidAttribute means the provided attribute is not valid
+	// ErrInvalidAttribute means the provided attribute is not valid.
 	ErrInvalidAttribute = errors.New("invalid attribute")
 )
